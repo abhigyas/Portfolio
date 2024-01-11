f423be73e41b613250ca22b5caaa4efeefc90489
Abhigya Sinha <sinha.abhigya0721@gmail.com>
2024-01-10 23:56:06 -0500

    quality of life changes
    -Added navigation without id
    -Added smooth scroll

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
+const aboutLink = document.querySelector('.about-link')
+aboutLink.addEventListener('click', () =>{
+    document.querySelector('.about-page').scrollIntoView({ behavior: 'smooth' });
+})
+const experienceLink = document.querySelector('.experience-link')
+experienceLink.addEventListener('click', () =>{
+    document.querySelector('.experience-page').scrollIntoView({ behavior: 'smooth' });
+})
+const contactLink = document.querySelector('.contact-link')
+contactLink.addEventListener('click', () =>{
+    document.querySelector('.contact-page').scrollIntoView({ behavior: 'smooth' });
+})
 function toggleMenu(){
         const menu = document.querySelector(".menu-links");
         const icon = document.querySelector(".hamburger-icon");
         menu.classList.toggle("open");
         icon.classList.toggle("open");
 }
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
